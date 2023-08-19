f59c582bcc73783bd784877a18f39ec13f11fce9
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-08-19 19:50:46 -0400

    ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,26 @@
-// Materia: Programación I, Paralelo 1
+// Materia: ProgramaciÃ³n I, Paralelo 1
 // Autor: Yerson Mateo Chui Mamani
-// Fecha creación: 17/08/2023
-// Fecha modificación: 17/08/2023
-// Número de ejericio: 8
-// Problema planteado: En el estacionamiento del Megacenter se cobra 7 Bs la primera hora, si pasa de una hora se cobra 5 Bs adicional por hora.
+// Fecha creaciÃ³n: 17/08/2023
+// Fecha modificaciÃ³n: 17/08/2023
+// NÃºmero de ejericio: 6
+// Problema planteado: numero primo
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b=0;
+    cout<<"ingrese el numero: ", cin>>a;
+    for(int i=1;i<=a;i++){
+            if(a%i==0){
+                b++;
+            }
+    }
+    if(b!=2){
+        cout<<"NO es numero primo"<< endl;
+    }
+    else{
+        cout<<"es numero primo"<< endl;
+    }
+    return 0;
+}
