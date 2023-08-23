700e6cb4b4522f606974737013f67874b26f88e9
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-08-23 08:26:28 -0400

    Update ejercicio 10.cpp

--- ejercicio 10.cpp
+++ ejercicio 10.cpp
@@ -1,22 +1,36 @@
-// Materia: Programación I, Paralelo 1
+// Materia: ProgramaciÃ³n I, Paralelo 1
 // Autor: Yerson Mateo Chui Mamani
-// Fecha creación: 17/08/2023
-// Fecha modificación: 17/08/2023
-// Número de ejericio: 10
-// Problema planteado: Realice un programa que detecte si una letra ingresada el vocal o consonante. Si no es ninguna letra que diga que es carácter especial.
-#include <ctype.h>
-#include <stdio.h>
+// Fecha creaciÃ³n: 17/08/2023
+// Fecha modificaciÃ³n: 17/08/2023
+// NÃºmero de ejericio: 10
+// Problema planteado: Realice un programa que detecte si una letra ingresada el vocal o consonante. Si no es ninguna letra que diga que es carÃ¡cter especial.
+#include <iostream>
 
 using namespace std;
-
-int esVocal(char letra){
-    char letramin = tolower(letra);
-    char vocal[] ="aeiou";
-
-    for(int i=0;vocal[i];i++){
-        if(letramin==vocal[i]){
-            return 1;
+int main(){
+    char caracter;
+    cout<<"Ingrese un caracter: ";
+    cin >> caracter;
+    if ((caracter>='a',caracter<='z') || (caracter >= 'A',caracter <= 'Z')){
+        switch (caracter){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                cout<<"Es una vocal."<<endl;
+                break;
+            default:
+                cout<<"Es una consonante."<<endl;
+                break;
         }
+    }else{
+        cout << "No es una letra."<<endl;
     }
     return 0;
-    }
+}
